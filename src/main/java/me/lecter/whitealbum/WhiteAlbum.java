4428947c4e787109725eec56d5632f63b20a6a85
acheron <acheron@DESKTOP-FC8M2KJ>
2024-04-11 14:38:08 +0900

    version 1.0.2

--- src/main/java/me/lecter/whitealbum/WhiteAlbum.java
+++ src/main/java/me/lecter/whitealbum/WhiteAlbum.java
@@ -32,7 +32,7 @@ public class WhiteAlbum {
 	private static Configs configs;
 	private static List<RiotAccount> accounts;
 	public static final String NAME = "WhiteAlbum";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	public static final String[] DEVELOPERS = {"x/twitter@iameugeosword", "github@lecterkn"};
 
 	public static void main(String[] args) {
